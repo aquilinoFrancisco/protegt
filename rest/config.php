9cbb9b75b3af5c50ca9d601d6fc15de2741a480c
AQUILINO FRANCISCO <aquilino.francisco@gmail.com>
2021-06-11 16:09:28 -0500

    Update config.php

--- rest/config.php
+++ rest/config.php
@@ -4,7 +4,7 @@ $db = [
     'host' => 'localhost',
     'username' => 'root',
     'password' => '',
-    'db' => 'reproductor' //Cambiar al nombre de tu base de datos
+    'db' => 'alumno' 
 ];
 
 ?>
